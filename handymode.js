7cf7262612cee61f2061a4ea904e1344f4fc5592
Jerissimo <jrom.peter80@gmail.com>
2026-09-06 22:15:00 +0200

    Add files via upload

--- handymode.js
+++ handymode.js
@@ -1,9 +1,41 @@
 (function () {
-  var html = document.documentElement;
-  var ua = navigator.userAgent || "";
-  var uaDataMobile = !!(navigator.userAgentData && navigator.userAgentData.mobile === true);
-  var mobileUA = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i.test(ua);
-  var ipadOS = /Macintosh/i.test(ua) && navigator.maxTouchPoints > 1;
+  function matches(query) {
+    return window.matchMedia && window.matchMedia(query).matches;
+  }
 
-  html.classList.toggle("handymode", uaDataMobile || mobileUA || ipadOS);
-})();
+  function isHandymode() {
+    if (navigator.userAgentData && typeof navigator.userAgentData.mobile === 'boolean') {
+      return navigator.userAgentData.mobile;
+    }
+
+    var ua = navigator.userAgent || navigator.vendor || window.opera || '';
+
+    if (/Mobi|Android|iPhone|iPad|iPod|Windows Phone|Mobile/i.test(ua)) {
+      return true;
+    }
+
+    return Boolean(
+      navigator.maxTouchPoints > 1 &&
+      matches('(pointer: coarse)') &&
+      matches('(hover: none)')
+    );
+  }
+
+  function applyHandymode() {
+    document.documentElement.classList.toggle('handymode', isHandymode());
+  }
+
+  applyHandymode();
+  window.addEventListener('pageshow', applyHandymode);
+
+  if (window.matchMedia) {
+    ['(pointer: coarse)', '(hover: none)'].forEach(function (query) {
+      var media = window.matchMedia(query);
+      if (media.addEventListener) {
+        media.addEventListener('change', applyHandymode);
+      } else if (media.addListener) {
+        media.addListener(applyHandymode);
+      }
+    });
+  }
+}());
